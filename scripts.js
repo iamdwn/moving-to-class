5c5e1392bb1900c497ccb4f93a21b1ab0a9e122c
iamdwn <truongdinhdongduong@gmail.com>
2024-08-17 19:40:34 +0700

    feat: add count-down and fix auto-save

--- scripts.js
+++ scripts.js
@@ -1,27 +1,34 @@
+let saveCount = 0;
+let alertDismissCount = 0;
+let countdownTime; 
 
 function customAlert(message) {
-    console.log("Alert triggered: " + message);
     setTimeout(function() {
-        console.log("Alert auto-closed (simulating OK press)");
+        alertDismissCount++;
+        console.log("Alert dismissed: " + alertDismissCount + " times");
+    }, 1000);
+}
+
+function Save() {
+        countdownTime = 5;
+        const countdownInterval = setInterval(function() {
+        console.log(`Tự động đổi lớp trong ${countdownTime} giây...`);
+        countdownTime--;
+        if (countdownTime < 0) {
+            clearInterval(countdownInterval);
+            autoSave()
+        }
     }, 1000);
 }
 
 function autoSave() {
-    let countdownTime = 5; 
-
-    const countdownInterval = setInterval(function() {
-        console.log(`Tự động đổi lớp trong ${countdownTime} giây...`);
-        countdownTime--;
-
-        if (countdownTime < 0) {
-            clearInterval(countdownInterval);
-
             document.getElementById("ctl00_mainContent_btSave").addEventListener("click", function(event) {
-                event.preventDefault(); //Ngăn tải lại trang hoặc điều hướng
-                customAlert("Đang chuyển ...");
+                event.preventDefault(); // Ngăn tải lại trang hoặc điều hướng
             });
 
             document.getElementById("ctl00_mainContent_btSave").click();
+            saveCount++; // Increment save counter
+            console.log("Count: " + saveCount + " times");
 
             // Kiểm tra tiến trình
             const successElement = document.getElementById("successMessageId");
@@ -33,8 +40,9 @@ function autoSave() {
                     clearInterval(intervalId); // Dừng check
                 }
             }, 1000); 
-        }
-    }, 1000); 
+
+            // Gọi lại hàm autoSave sau khi hoàn thành để lặp lại
+            setTimeout(autoSave, 5000);
 }
 
-autoSave();
+Save();
