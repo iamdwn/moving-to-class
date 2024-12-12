1ac905af014e934e2b4780f5d5f0b955f520808b
iamdwn <truongdinhdongduong@gmail.com>
2024-12-12 23:39:32 +0700

    feat: update autosave.js

--- autosave.js
+++ autosave.js
@@ -3,6 +3,8 @@ const puppeteer = require('puppeteer-core');
 (async () => {
     const browser = await puppeteer.connect({
         browserURL: 'http://localhost:9222',
+        // defaultViewport: null,
+        // headless: true,
     });
 
     const pages = await browser.pages();
@@ -13,29 +15,55 @@ const puppeteer = require('puppeteer-core');
         console.log(`Alert: ${message}`);
 
         if (message.includes('Bạn không thể chuyển tới lớp này')) {
-            console.log('Failed. Moving ...');
+            console.log('Failed.\n==========================');
             await dialog.accept(); 
-            
+
             await new Promise(resolve => setTimeout(resolve, 1000)); 
 
             const saveButton = await page.$('input[value="Save"]');
             if (saveButton) {
-                console.log("Saving ...");
-                await saveButton.click();
+                console.log("Retrying ...");
+
+                const timeout = new Promise((_, reject) => setTimeout(() => reject(new Error("Timeout reached, exiting program.")), 3000));
+
+                try {
+                    await Promise.race([
+                        saveButton.click(),
+                        timeout
+                    ]);
+                } catch (error) {
+                    console.error(error.message);
+                    await browser.close();
+                    process.exit(1);  
+                }
             }
         } else {
-            console.log('Successfully !!\n Closing ...');
+            console.log('Successfully.\nClosing ...');
             await dialog.accept();
-            await browser.close(); 
+            await browser.close();
+            process.exit(0); 
         }
     });
 
     const saveButton = await page.$('input[value="Save"]');
     if (saveButton) {
-        console.log("Saving ...");
-        await saveButton.click();
+        console.log("Moving ...");
+        const timeout = new Promise((_, reject) => setTimeout(() => reject(new Error("Timeout reached, exiting program.")), 3000));
+
+        try {
+            await Promise.race([
+                saveButton.click(),
+                timeout
+            ]);
+        } catch (error) {
+            console.error(error.message);
+            await browser.close();
+            process.exit(1);  
+        }
     } else {
         console.error("Error save");
+        await browser.close();
+        process.exit(1);  
     }
 
     console.log("Moving out class");
